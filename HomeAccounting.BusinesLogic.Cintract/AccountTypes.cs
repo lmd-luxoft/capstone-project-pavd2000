45dd7fb136ebc6092cc34a894e51e229cec3d48f
Pavel Danilychev <pavd2000@yandex.ru>
2021-06-07 22:14:53 +0300

    Бизнес сущности

---
+++ HomeAccounting.BusinesLogic.Cintract/AccountTypes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeAccounting.BusinesLogic.Contract
+{
+    /// <summary>
+    /// Тип счета. Активы = Пассивы + Капитал
+    /// </summary>
+    public enum AccountTypes
+    {
+        // Активы (тип дебит)
+        Assets,
+        // Пассивы (Обязательства) (тип кредит)
+        Liabilities,
+        // Собственный капитал (тип кредит)
+        Equity,
+        // Доходы (тип дебит)
+        Income,
+        // Расходы (тип кредит)
+        Expenses
+
+    }
+}
---
+++ HomeAccounting.BusinesLogic.Cintract/TransactionsFilter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeAccounting.BusinesLogic.Contract
+{
+    /// <summary>
+    /// Фильтр для выбора проводок
+    /// </summary>
+    public class TransactionsFilter
+    {
+        public AccountTypes CreditAccountType { get; set; }
+        public AccountTypes DebetAccountType { get; set; }
+
+        public Account CreditAccount { get; set; }
+
+        public Account DebetAccount { get; set; }
+    }
+}
