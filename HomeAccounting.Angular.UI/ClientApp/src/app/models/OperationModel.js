c91404b592a7947404035738e66e60dc4c77ad29
Pavel Danilychev <pavd2000@yandex.ru>
2021-06-13 22:33:43 +0300

    Добавлен проект на Angular

---
+++ HomeAccounting.Angular.UI/ClientApp/src/app/models/OperationModel.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.OperationModel = void 0;
+var OperationModel = /** @class */ (function () {
+    function OperationModel() {
+    }
+    return OperationModel;
+}());
+exports.OperationModel = OperationModel;
+//# sourceMappingURL=OperationModel.js.map
